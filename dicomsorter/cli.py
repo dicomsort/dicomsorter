7d31b83bf48b4b100642c8aa304ac4534c9fb3f0
Jonathan Suever <jonathan.suever@rigor.com>
2018-12-17 18:33:56 -0500

    Add --version argument to CLI

--- dicomsorter/cli.py
+++ dicomsorter/cli.py
@@ -8,6 +8,7 @@ import os
 import sys
 import textwrap
 
+from . import __version__
 from config import logger
 from dicom_utils import available_fields
 from dicomsorter import DICOMSorter
@@ -20,83 +21,90 @@ def run():
     )
 
     parser.add_argument(
+        '--version',
+        action='version',
+        version='%%(prog)s %s' % __version__,
+        help='display the current version and exit'
+    )
+
+    parser.add_argument(
         'input_directory',
         type=os.path.abspath,
-        help='Directory to recursively search for DICOM images'
+        help='directory to recursively search for DICOM images'
     )
 
     parser.add_argument(
         'output_directory',
         type=os.path.abspath,
-        help='Directory in which to place sorted DICOM images'
+        help='directory in which to place sorted DICOM images'
     )
 
     parser.add_argument(
         '--path',
         nargs='+',
         default=['SeriesDescription'],
-        help='A list of dicom fields to sort images by. Example: --path PatientName SeriesDescription'
+        help='a list of dicom fields to sort images by. Example: --path PatientName SeriesDescription'
     )
 
     parser.add_argument(
         '--filename-format',
         default='%(ImageType)s (%(InstanceNumber)04d)%(Extension)s',
-        help='Format to use for the filename'
+        help='format to use for the filename'
     )
 
     parser.add_argument(
         '--anonymize',
         action='store_true',
-        help='Anonymize the output images'
+        help='anonymize the output images'
     )
 
     parser.add_argument(
         '--overwrite',
         action='store_true',
-        help='Overwrite files in the destination if they exist'
+        help='overwrite files in the destination if they exist'
     )
 
     parser.add_argument(
         '--list-fields',
         action='store_true',
-        help='List available DICOM fields and exit'
+        help='list available DICOM fields and exit'
     )
 
     parser.add_argument(
         '--verbose',
         action='store_true',
-        help='Show verbose output to the command line'
+        help='show verbose output to the command line'
     )
 
     parser.add_argument(
         '--dry-run',
         action='store_true',
-        help='Do not actually save the resulting images'
+        help='do not actually save the resulting images'
     )
 
     parser.add_argument(
         '--original-filename',
         action='store_true',
-        help='Use the original filename'
+        help='use the original filename'
     )
 
     parser.add_argument(
         '--concurrency',
         type=int,
         default=2,
-        help='Number of threads to perform sorting (default: 2)'
+        help='number of threads to perform sorting (default: 2)'
     )
 
     parser.add_argument(
         '--move',
         action='store_true',
-        help='Move the original files rather than copying them'
+        help='move the original files rather than copying them'
     )
 
     parser.add_argument(
         '--force',
         action='store_true',
-        help='Automatically accept any prompts'
+        help='automatically accept any prompts'
     )
 
     arguments = parser.parse_args()
