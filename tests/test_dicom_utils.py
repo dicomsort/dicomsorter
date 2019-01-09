9e7be5c6117a13308222f99ea0ac830401004ba9
Jonathan Suever <jonathan.suever@rigor.com>
2019-01-08 19:52:55 -0500

    Add some tests for dicom_utils

--- tests/test_dicom_utils.py
+++ tests/test_dicom_utils.py
@@ -1,8 +1,91 @@
 """Unit tests for dicom_utils module."""
+import functools
+import os
+import tempfile
 
+import pytest
 from pydicom import Dataset
 
-from dicomsorter.dicom_utils import age_in_years
+from dicomsorter.dicom_utils import age_in_years, available_fields, dicom_list
+from dicomsorter.errors import DicomsorterException
+
+from .factories import DicomFactory
+
+
+@pytest.fixture
+def dicom_folder():
+    folder = tempfile.mkdtemp(prefix="dicomsorter-test-")
+
+    # Number of images to create
+    def generate(number=5):
+        files = []
+
+        for k in range(number):
+            filename = os.path.join(folder, "Image%04d" % k)
+            dicom = DicomFactory.build_with_defaults()
+            dicom.save_as(filename, write_like_original=False)
+            files.append(filename)
+
+        return {"folder": folder, "files": files}
+
+    return generate
+
+
+@pytest.fixture
+def dicom_file():
+    filename = tempfile.mktemp()
+
+    def save(**kwargs):
+        dicom = DicomFactory.build(**kwargs)
+        dicom.save_as(filename, write_like_original=False)
+
+        yield filename
+
+        os.unlink(filename)
+
+    return save
+
+
+class TestDicomList:
+    def test_when_no_dicoms(self, dicom_folder):
+        params = dicom_folder(number=0)
+        message = 'No valid DICOMs found in "%(folder)s".' % params
+
+        with pytest.raises(DicomsorterException, message=message):
+            list(dicom_list(params["folder"]))
+
+    def test_when_all_dicoms(self, dicom_folder):
+        params = dicom_folder(number=5)
+        all_dicoms = list(dicom_list(params["folder"]))
+
+        assert len(all_dicoms) == 5
+
+        filenames = map(lambda x: x.filename, all_dicoms)
+        filenames.sort()
+        params["files"].sort()
+
+        assert filenames == params["files"]
+
+    def test_when_invalid_directory(self):
+        directory = "/not/a/directory"
+        message = 'Directory "%s" does not exist.' % directory
+        with pytest.raises(DicomsorterException, message=message):
+            list(dicom_list(directory))
+
+
+class TestAvailableFields:
+    def test_when_is_dicom(self):
+        fields = {
+            "PatientName": "Patient Name",
+            "PatientID": "123456",
+            "StudyDate": "20190101",
+        }
+
+        dicom = DicomFactory.build(**fields)
+
+        field_names = fields.keys()
+        field_names.sort()
+        assert dicom.dir() == field_names
 
 
 class TestAgeInYears:
@@ -10,9 +93,8 @@ class TestAgeInYears:
         assert age_in_years(Dataset()) == ""
 
     def test_when_patient_age(self):
-        dicom = Dataset()
         age = "100Y"
-        dicom.update({"PatientAge": age})
+        dicom = DicomFactory.build(PatientAge=age)
 
         assert age_in_years(dicom) == age
 
@@ -25,7 +107,5 @@ class TestAgeInYears:
         assert age_in_years(dicom) == expected
 
     def test_when_no_birth_date(self):
-        dicom = Dataset()
-        dicom.update({"StudyDate": "20181217"})
-
+        dicom = DicomFactory.build(StudyDate="20181217")
         assert age_in_years(dicom) == ""
