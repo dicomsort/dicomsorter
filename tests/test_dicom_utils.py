3a115a668986038533fdc861fe6c50859faaa50a
Jonathan Suever <jonathan.suever@rigor.com>
2019-01-09 06:31:40 -0500

    Add tests that require dicomdir

--- tests/test_dicom_utils.py
+++ tests/test_dicom_utils.py
@@ -1,12 +1,11 @@
 """Unit tests for dicom_utils module."""
 import pytest
 import os
-import functools
 import tempfile
 
 from pydicom import Dataset
-from .factories import DicomFactory
-from dicomsorter.dicom_utils import age_in_years, available_fields, dicom_list
+from .factories import DicomFactory, DicomDirFactory
+from dicomsorter.dicom_utils import age_in_years, available_fields, dicom_list, has_dicm_prefix, is_dicom
 from dicomsorter.errors import DicomsorterException
 
 
@@ -15,13 +14,20 @@ def dicom_folder():
     folder = tempfile.mkdtemp(prefix='dicomsorter-test-')
 
     # Number of images to create
-    def generate(number=5):
+    def generate(number=5, dicomdir=False):
         files = []
 
         for k in range(number):
             filename = os.path.join(folder, 'Image%04d' % k)
             dicom = DicomFactory.build_with_defaults()
             dicom.save_as(filename, write_like_original=False)
+            files.append(filename)
+
+        # Add DicomDir file
+        if dicomdir:
+            dicomdir = DicomDirFactory.build()
+            filename = os.path.join(folder, 'DICOMDIR')
+            dicomdir.save_as(filename, write_like_original=False)
             files.append(filename)
 
         return {
@@ -33,18 +39,15 @@ def dicom_folder():
 
 
 @pytest.fixture
-def dicom_file():
+def dicom_file(**kwargs):
     filename = tempfile.mktemp()
 
-    def save(**kwargs):
-        dicom = DicomFactory.build(**kwargs)
-        dicom.save_as(filename, write_like_original=False)
+    dicom = DicomFactory.build_with_defaults(**kwargs)
+    dicom.save_as(filename, write_like_original=False)
 
-        yield filename
+    yield filename
 
-        os.unlink(filename)
-
-    return save
+    os.unlink(filename)
 
 
 class TestDicomList:
@@ -75,7 +78,7 @@ class TestDicomList:
 
 
 class TestAvailableFields:
-    def test_when_is_dicom(self):
+    def test_when_is_dicom(self, tmpdir):
         fields = {
             'PatientName': 'Patient Name',
             'PatientID': '123456',
@@ -83,14 +86,14 @@ class TestAvailableFields:
         }
 
         dicom = DicomFactory.build(**fields)
+        dicom.save_as(os.path.join(tmpdir, 'image.dcm'), write_like_original=False)
 
         field_names = list(fields.keys())
         field_names.sort()
-        assert dicom.dir() == field_names
+        assert available_fields(tmpdir) == field_names
 
 
 class TestAgeInYears:
-
     def test_when_no_age_or_birthday(self):
         assert age_in_years(Dataset()) == ''
 
@@ -111,3 +114,55 @@ class TestAgeInYears:
     def test_when_no_birth_date(self):
         dicom = DicomFactory.build(StudyDate='20181217')
         assert age_in_years(dicom) == ''
+
+    def test_when_empty_birth_date(self):
+        dicom = DicomFactory.build(StudyDate='20181217', PatientBirthDate='')
+        assert age_in_years(dicom) == ''
+
+
+class TestIsDicom:
+    def test_non_dicom(self, tmpdir):
+        filename = os.path.join(tmpdir, 'invalid')
+        with open(filename, 'wb') as fid:
+            fid.write(b'')
+
+        assert(is_dicom(filename) == False)
+
+    def test_permissions_error(self, dicom_file):
+        os.chmod(dicom_file, 0)
+
+        assert(is_dicom(dicom_file) == False)
+
+    def test_with_dicomdir(self, tmpdir):
+        filename = os.path.join(tmpdir, 'DICOMDIR')
+        dicomdir = DicomDirFactory.build()
+        dicomdir.save_as(filename, write_like_original=False)
+
+        assert(is_dicom(filename) == False)
+
+    def test_with_load_dicomdir(self, tmpdir):
+        filename = os.path.join(tmpdir, 'DICOMDIR')
+        dicomdir = DicomDirFactory.build()
+        dicomdir.save_as(filename, write_like_original=False)
+
+        assert(is_dicom(filename, ignore_dicomdir=False) != False)
+
+
+class TestHasDICMPrefix:
+    def test_no_prefix(self, tmpdir):
+        filename = os.path.join(tmpdir, 'invalid')
+        preamble = b'\x00' * 128
+        with open(filename, 'wb') as fid:
+            fid.write(preamble + b'DACM')
+
+        assert(has_dicm_prefix(filename) == False)
+
+    def test_empty_file(self, tmpdir):
+        filename = os.path.join(tmpdir, 'empty')
+        with open(filename, 'wb') as fid:
+            fid.write(b'')
+
+        assert(has_dicm_prefix(filename) == False)
+
+    def test_with_valid_dicom(self, dicom_file):
+        assert(has_dicm_prefix(dicom_file) == True)
